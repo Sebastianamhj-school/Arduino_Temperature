3bb2401966cbe0397be067664597f78cf5cddbff
Sebastianamhj-school <78901791+Sebastianamhj-school@users.noreply.github.com>
2022-09-09 09:44:45 +0200

    Update ard_temp.ino

--- ard_temp.ino
+++ ard_temp.ino
@@ -1,12 +1,15 @@
 #include <Wire.h>
 
-#define BLED 2        // Blue LED
-#define YLED 3        // Yellow LED
-#define RLED 4        // Red LED
-#define ADDRESS 0x48  // DS1621 Address
+/// Blue LED
+const int BLED = 2;
+/// Yellow LED
+const int YLED = 3;
+/// Red LED
+const int RLED = 4;
+/// DS1621 Address
+const byte ADDRESS = 0x48;
 
-int lastAnalogTemp = 0;
-
+/// Setup the program
 void setup() {
   Wire.begin();
   Serial.begin(9600);
@@ -16,11 +19,15 @@ void setup() {
   tempSetup(ADDRESS);
 }
 
+/// Loops through, reading the temperature from the DS1621
+/// and the input value from the potentiometer, to set a desired temperature
+/// afterwards displays the values on the serial and LED's.
+/// It have a 0.5sec delay, if it was any longer it would give too much lag to the
+/// potentiometer, and there is no need for it to be any lower.
 void loop() {
-  int tempInputRaw = analogTempReader(10);
-  int tempInputMapped = (map(tempInputRaw, 0, 1023, 0, 300)/5)*5;  /// Round the reading value to the nearest 5,
+  int tempInputRaw = potentiometerReader(10);
   
-  double currentInputTemp = (double)tempInputMapped/10;
+  double currentInputTemp = (double)tempInputRaw/10;
   double currentTemp = tempRead(ADDRESS);
   
   Serial.print("Temp: ");
@@ -33,26 +40,33 @@ void loop() {
   delay(500);
 }
 
-/// Reads the potentiometer, to get the wanted tempeture
-int analogTempReader(int tempBuffer){
+/// Reads the potentiometer on the port A0, and returns a value to the nearest 5
+/// @param  tempBuffer  set the buffer value for the potentiometer so it doesn't fluctuates.
+/// @return returns a value to the nearest 5
+int potentiometerReader(int tempBuffer){
+  static int lastReading;
   int reader = analogRead(A0);
-  if(reader > lastAnalogTemp + tempBuffer || reader < lastAnalogTemp - tempBuffer ){
-    lastAnalogTemp = reader;
-    return lastAnalogTemp;
+  
+  if(reader > lastReading + tempBuffer || reader < lastReading - tempBuffer ){
+    lastReading = reader;
+    return (map(lastReading, 0, 1023, 0, 300)/5) * 5;
   }
 }
 
 /// Setup the temperature sensor DS1621
+/// @param  address The byte address of the DS1621.
 void tempSetup(byte address){
   Wire.beginTransmission(address);
-  Wire.write(0xAC);                   /// Access Config
+  Wire.write(0xAC);
   Wire.write(0);
   Wire.beginTransmission(address);
-  Wire.write(0xEE);                   /// Start Convert
+  Wire.write(0xEE);
   Wire.endTransmission();
 }
 
 /// Reads the temperate from the DS1621.
+/// @param  address The byte address of the DS1621.
+/// @return returns the temperature that the DS1621 measures.
 double tempRead(int address){
   Wire.beginTransmission (address);
   Wire.write (0xAA);
@@ -72,13 +86,15 @@ double tempRead(int address){
 }
 
 /// Handle the LED depending on temperature and the wanted temperature.
-void setLights(int currentTemp, int wantedTemp){
-  if(currentTemp > wantedTemp){
+/// @param  currentTemp The temperature read from the DS1621.
+/// @param  desiredTemp The desired temperature set from the potetiometer.
+void setLights(int currentTemp, int desiredTemp){
+  if(currentTemp > desiredTemp){
     digitalWrite(BLED, LOW);
     digitalWrite(YLED, LOW);
     digitalWrite(RLED, HIGH);
   }
-  else if(currentTemp < wantedTemp){
+  else if(currentTemp < desiredTemp){
     digitalWrite(BLED, HIGH);
     digitalWrite(YLED, LOW);
     digitalWrite(RLED, LOW);
